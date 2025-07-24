f4a1b93d6dccf2b0873826f5c0e8d0ef668689b3
gyu-hyung <gud5603@gmail.com>
2025-07-24 11:21:39 +0900

    teen_now_ui_test

--- lib/src/ui/pages/teen_now/teen_now_page.dart
+++ lib/src/ui/pages/teen_now/teen_now_page.dart
@@ -32,12 +32,12 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
   late final ScrollController scrollController;
   //floating buttons
   late final ValueNotifier<double> _sheetHeight;
-  final double panelOpenHeight = 440.0;
-  final double panelCloseHeight = 150.0;
 
-  double anchor = 0.2;
+  double anchor = 0.28;
   double minBound = 0;
   double upperBound = 1.0;
+  //panel
+  bool showFriendDetail = false;
 
   @override
   void dispose() {
@@ -56,8 +56,8 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
 
     WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {
           upperBound = 0.6;
-          minBound = 0.2;
-          anchor = 0.2;
+          minBound = 0.28;
+          anchor = 0.28;
         }));
   }
 
@@ -140,7 +140,10 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
         return AnimatedPositioned(
           duration: const Duration(milliseconds: 200),
           curve: Curves.easeInOut,
-          bottom: value + 16.0,
+          bottom: ((value) *
+                  (MediaQuery.of(context).size.height -
+                      (MediaQuery.of(context).size.height / 5))) +
+              16,
           right: 16,
           child: Column(
             children: [
@@ -181,7 +184,6 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
       anchor: anchor,
       minimumBound: minBound,
       upperBound: upperBound,
-      //드래그로 시트 hide
       dragEnd: (details) {
         final velocity = details.velocity.pixelsPerSecond.dy;
 
@@ -195,13 +197,13 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
         if (panelController.status != SlidingUpPanelStatus.dragging)
           {
             if (panelController.status == SlidingUpPanelStatus.expanded)
-              {_sheetHeight.value = panelOpenHeight}
+              {_sheetHeight.value = upperBound}
             else if (panelController.status == SlidingUpPanelStatus.anchored ||
                 panelController.status == SlidingUpPanelStatus.collapsed)
-              {_sheetHeight.value = panelCloseHeight}
+              {_sheetHeight.value = minBound}
             else
               {_sheetHeight.value = 0.0}
-          }
+          },
       },
       child: Container(
         decoration: const ShapeDecoration(
@@ -229,19 +231,21 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
               builder: (context, value, child) {
                 return AnimatedCrossFade(
                   duration: const Duration(milliseconds: 150),
-                  crossFadeState: value > panelCloseHeight
+                  crossFadeState: value <= minBound
                       ? CrossFadeState.showFirst
                       : CrossFadeState.showSecond,
-                  firstChild: SizedBox(
-                    height: 600, // 원하는 패널 최대 높이
-                    child: Column(
-                      children: [
-                        _buildCountWithAddFriends(),
-                        Expanded(child: _buildFriendList()),
-                      ],
-                    ),
+                  firstChild: _buildProfileSection(),
+                  secondChild: SizedBox(
+                    height: 600,
+                    child: showFriendDetail
+                        ? _buildFriendDetail()
+                        : Column(
+                            children: [
+                              _buildCountWithAddFriends(),
+                              Expanded(child: _buildFriendList()),
+                            ],
+                          ),
                   ),
-                  secondChild: _buildProfileSection(),
                   firstCurve: Curves.easeIn,
                   secondCurve: Curves.easeOut,
                   sizeCurve: Curves.easeInOut,
@@ -269,87 +273,9 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
     );
   }
 
-  Widget _buildCountWithAddFriends() {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
-          height: 53,
-          child: Row(
-            children: [
-              const Text(
-                '🥳 친구 53',
-                style: TextStyle(
-                    fontFamily: kPaperlogyFont,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500),
-              ),
-              const Spacer(),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: const Color(0xff1947E5),
-                  foregroundColor: Colors.black,
-                  side: const BorderSide(color: Colors.black, width: 2),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                  textStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                onPressed: () {},
-                child: const Row(
-                  children: [
-                    Icon(
-                      Icons.add_circle_outline,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      '친구추가',
-                      style: TextStyle(
-                        fontSize: 11,
-                        color: Colors.white,
-                        fontFamily: kPaperlogyFont,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        const Divider(
-          height: 1.0,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildFriendList() {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 188),
-      child: ListView.separated(
-        controller: scrollController,
-        physics: const ClampingScrollPhysics(),
-        itemBuilder: (context, index) {
-          return _buildProfileSection();
-        },
-        separatorBuilder: (context, index) {
-          return const Divider(height: 0.5);
-        },
-        itemCount: 10,
-      ),
-    );
-  }
-
   Widget _buildProfileSection() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(22, 22, 22, 16),
+      padding: const EdgeInsets.fromLTRB(22, 16, 22, 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -433,5 +359,177 @@ class _TeenNowPageViewState extends State<TeenNowPageView> {
         ],
       ),
     );
+  }
+
+  Widget _buildCountWithAddFriends() {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.topCenter,
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 12),
+          height: 42,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Row(
+                children: [
+                  Text(
+                    '😊',
+                    style: TextStyle(fontSize: 13),
+                  ),
+                  SizedBox(width: 6),
+                  Text(
+                    '친구 53',
+                    style: TextStyle(
+                      fontFamily: kPaperlogyFont,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              SizedBox(
+                width: 77,
+                height: 30,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: const Color(0xff1947E5),
+                    side: BorderSide.none,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.zero,
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  onPressed: () {},
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.add_circle_outline,
+                        size: 12,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        '친구추가',
+                        style: TextStyle(
+                          fontSize: 11,
+                          color: Colors.white,
+                          fontFamily: kPaperlogyFont,
+                          fontWeight: FontWeight.w600,
+                          height: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(height: 1.0),
+      ],
+    );
+  }
+
+  Widget _buildFriendList() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 188),
+      child: ListView.separated(
+        controller: scrollController,
+        physics: const ClampingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return _buildFriendListItem();
+        },
+        separatorBuilder: (context, index) {
+          return const Divider(height: 0.5);
+        },
+        itemCount: 10,
+      ),
+    );
+  }
+
+  Widget _buildFriendListItem() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(22, 22, 22, 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 52,
+            height: 52,
+            decoration: const BoxDecoration(
+              color: Color(0xFF9969FF),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(32)),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: Transform.scale(
+              scale: 1.3,
+              child: Image.asset(
+                'assets/images/ic_darcy_avartar1.png',
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          const Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '강윤슬(눈물의달력) 🎉',
+                style: TextStyle(
+                    fontFamily: kPaperlogyFont, fontWeight: FontWeight.w500),
+              ),
+              Text(
+                '😍 오늘도 맑음',
+                style: TextStyle(
+                    fontFamily: kPaperlogyFont,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff949494)),
+              ),
+              Text(
+                '광주 북구 오룡동',
+                style: TextStyle(
+                    fontFamily: kPaperlogyFont,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff949494)),
+              ),
+            ],
+          ),
+          const Spacer(),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.amber,
+              foregroundColor: Colors.black,
+              side: const BorderSide(color: Colors.black, width: 2),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(14),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+            ),
+            onPressed: () {},
+            child: const Text(
+              '프로필',
+              style: TextStyle(
+                fontFamily: kPaperlogyFont,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFriendDetail() {
+    return Container();
   }
 }
